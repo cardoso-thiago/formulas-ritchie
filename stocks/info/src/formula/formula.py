8dc2582e2e99bee21f4561b38ac454a2c3d48c53
cardoso-thiago <thiago.cardoso33@gmail.com>
2021-02-16 19:27:21 -0300

    Melhoria na obtenção das informações do Yahoo Finance

--- stocks/info/src/formula/formula.py
+++ stocks/info/src/formula/formula.py
@@ -7,33 +7,42 @@ from yahoo_fin import stock_info as si
 round_number = 2
 
 def Run(ticker1, ticker2, ticker3, ticker4, ticker5):
-    print_ticker_info(ticker1)
-    print_ticker_info(ticker2)
-    print_ticker_info(ticker3)
-    print_ticker_info(ticker4)
-    print_ticker_info(ticker5)
+    validate_ticker(ticker1)
+    validate_ticker(ticker2)
+    validate_ticker(ticker3)
+    validate_ticker(ticker4)
+    validate_ticker(ticker5)
+
+def validate_ticker(ticker):
+    if ticker is not None:
+        print_ticker_info(ticker)
 
 def print_ticker_info(ticker):
-    if ticker is not None:
-        ticker_price = si.get_live_price(ticker)
-        output = ticker.split('.', 1)[0] + ': ' + str(round(ticker_price, round_number))
-        print(f"{fg(3)}{output}{get_dividends(ticker)}{attr(0)}")
-        print_change(ticker, ticker_price)
+    dict_quote = si.get_quote_table(ticker, dict_result = True)
+    current_price = dict_quote.get('Quote Price')
+    previous_close = dict_quote.get('Previous Close')
+    change = current_price - previous_close
+    
+    change_percentage = "{}%".format(round(100 - (previous_close*100/current_price), round_number))
+    change_e = round(change, round_number)
+    current_price_e = ticker.split('.', 1)[0] + ': ' + str(round(current_price, round_number))
+    dividends = get_dividends(ticker)
+    change_color = 3
+    change_signal = ""
+
+    if(change > 0):
+        change_color = 2
+        change_signal = "+"
+    else:
+        change_color = 1
+
+    print(f"{fg(3)}{current_price_e} {fg(change_color)}{change_signal}{change_e}({change_percentage}){fg(3)}{dividends}{attr(0)}")
 
 def get_dividends(ticker):
     try:
         today = date.today()
         last_year = today + relativedelta(years=-1)
         dividends = si.get_dividends(ticker, start_date = last_year, end_date = today, index_as_date = True)
-        return " - Dividends (12 months): {}".format(round(dividends['dividend'].sum(), round_number))
+        return " => Dividends (12 months): {}".format(round(dividends['dividend'].sum(), round_number))
     except Exception:
         return ""
-
-def print_change(ticker, ticker_price):
-    previous_close = si.get_quote_table(ticker, dict_result = True).get('Previous Close')
-    change = ticker_price - previous_close
-    change_percentage = "{}%".format(round(100 - (previous_close*100/ticker_price), round_number))
-    if(change > 0):
-        print(f"  => Change: {fg(2)}{round(change, round_number)} ({change_percentage}){attr(0)}")
-    else:
-        print(f"  => Change: {fg(1)}{round(change, round_number)} ({change_percentage}){attr(0)}")
