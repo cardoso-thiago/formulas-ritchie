26eef8c0adb77816029e0d6bbaf89e27c11210b4
cardoso-thiago <thiago.cardoso33@gmail.com>
2021-02-21 13:03:20 -0300

    Alterando a lista de seleção do Gitlab para usar o inquirer

--- gitlab/wiki/src/formula/formula.py
+++ gitlab/wiki/src/formula/formula.py
@@ -2,6 +2,7 @@
 import requests, re, sys, subprocess, os
 from colored import fg, attr
 from bs4 import BeautifulSoup
+import inquirer
 
 def Run():
     url = 'https://cardoso-thiago.gitlab.io/site-antora-demo/'
@@ -10,37 +11,25 @@ def Run():
     page = requests.get(url + location)
     soup = BeautifulSoup(page.content, 'html.parser')
     article = soup.find('article')
-    count = 0
+
     dict_links = {} 
     for link in article.find_all('a'):
         finalUrl = url + location.replace('main.html', link.get('href')) 
         if not finalUrl in dict_links.values():
-            count+=1
-            dict_links[count] = finalUrl
-            print("[{}] - {}".format(count, link.get_text()))
-    user_selection = get_user_selection(dict_links)
-    url_selection = dict_links.get(user_selection)
+            dict_links[link.get_text()] = finalUrl
+
+    gitlab_links = [
+    inquirer.List('gitlab_links', 
+        message = "Qual página você quer visualizar?",
+        choices = dict_links,
+        carousel = True
+        ),
+    ]
+
+    user_selection = inquirer.prompt(gitlab_links)
+    url_selection = dict_links.get(user_selection["gitlab_links"])
     print(f"{fg(2)}Abrindo a url {url_selection}{attr(0)}")
     open_url(url_selection)
-
-def get_user_selection(dict_links):
-    if sys.version_info.major == 2:
-        selection = raw_input()
-    elif sys.version_info.major == 3:
-        selection = input()
-
-    try:
-        if int(selection) < 1:
-            print(f"{fg(1)}Insira uma opção maior que 0:{attr(0)}")
-            return get_user_selection(dict_links)
-        elif int(selection) > len(dict_links):
-            print(f"{fg(1)}Insira uma opção menor ou igual a {str(len(dict_links))}:{attr(0)}")
-            return get_user_selection(dict_links)
-        else:
-            return int(selection)
-    except ValueError:
-        print(f"{fg(1)}Insira um valor entre 1 e {str(len(dict_links))}:{attr(0)}")
-        return get_user_selection(dict_links)
 
 def open_url(url):
     if sys.platform == 'win32':
